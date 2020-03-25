dd07ecadeacef25ecfa8fe86173206cc20b787a0
suno-opnasd <8435528+suno-opnasd@users.noreply.github.com>
2020-03-25 06:05:39 +0000

    adding pelican files and first post

---
+++ content/page01.md
@@ -0,0 +1,11 @@
+Title: Welcome to datemega.net
+Date: 2020-03-25
+Category: Portal
+Tags: first,test
+Slug: Welcome
+Authors: shoh8
+Summary: First post by Pelican in datemega.net
+
+## datemega.net 
+
+Hello **world!**
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'shoh8'
+SITENAME = 'datemega.net'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'ja'
+
+THEME = 'theme'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/shoh8'),
+          ('github', 'https://github.com/shoh8'),)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
