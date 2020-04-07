b0d0aba695fc961949ceb2a0c7da332b31227c62
suno-opnasd <8435528+suno-opnasd@users.noreply.github.com>
2020-04-07 17:32:28 +0900

    pelicanconf modified

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'shoh8'
-SITENAME = 'datemega.net'
+SITENAME = 'blog.datemega.net'
 SITEURL = ''
 
 PATH = 'content'
@@ -22,10 +22,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('BB is watching you', '#'),)
 
 # Social widget
 SOCIAL = (('Twitter', 'https://twitter.com/shoh8'),
@@ -36,8 +33,8 @@ DEFAULT_PAGINATION = 5
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-STATIC_PATHS = { 'extra/CNAME' }
 
+STATIC_PATHS = { 'extra' , 'images', 'static'}
 EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'},
 }
